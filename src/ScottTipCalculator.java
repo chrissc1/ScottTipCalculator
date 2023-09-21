40e8396bfbd96e63b61e1358ed6a8733a7b80030
BT_2N2_22 <sample@bths.edu>
2023-09-21 13:10:29 -0400

    initial commit

---
+++ src/ScottTipCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ScottTipCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How much people are in your group? : ");
+        String party = scan.nextLine();
+        System.out.println("Tip percentage?");
+        int tipPercent = scan.nextInt();
+
+        System.out.println("Name the 4 items ")
+
+    }
+}
