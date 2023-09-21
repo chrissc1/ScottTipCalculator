4f47fedc7ba86d2e1d1d2962264323200f58f700
BT_2N2_22 <sample@bths.edu>
2023-09-21 13:22:40 -0400

    changed alot

--- src/ScottTipCalculator.java
+++ src/ScottTipCalculator.java
@@ -7,9 +7,28 @@ public class ScottTipCalculator {
         System.out.println("How much people are in your group? : ");
         String party = scan.nextLine();
         System.out.println("Tip percentage?");
-        int tipPercent = scan.nextInt();
+        double tipPercent = scan.nextDouble();
 
-        System.out.println("Name the 4 items ")
+        scan.nextLine();
+
+        System.out.println("Name an item you want to eat: ");
+        String item1 = scan.nextLine();
+        System.out.println("What is the cost of this item: ");
+        double item1cost = scan.nextDouble();
+        System.out.println("Name another item you want to eat: ");
+        String item2 = scan.nextLine();
+        System.out.println("What is the cost of this item: ");
+        double item2cost = scan.nextDouble();
+        System.out.println("Name another item you want to eat: ");
+        String item3 = scan.nextLine();
+        System.out.println("What is the cost of this item: ");
+        double item3cost = scan.nextDouble();
+        System.out.println("Name another item you want to eat: ");
+        String item4 = scan.nextLine();
+        System.out.println("What is the cost of this item: ");
+        double item4cost = scan.nextDouble();
+
+        tipPercent *= (0. 01);
 
     }
 }
